06c5a4f8052b7953fdbed9515f1087ca11c96c82
meow0516 <76482269+meow0516@users.noreply.github.com>
2021-08-09 00:07:31 +0800

    + list content save into array

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
 $(document).ready(function() {
     
-    // function blur_todo() {
-    //     console.log('blur');
-    //     // $(this).text(current_content);
-    //     // $(this).siblings('.edit').removeClass('icon-floppy').addClass('icon-pencil');
-    //     // $(this).attr('contenteditable','false');
-    // }
+    
     let current_content = '';
+    let content_arr = [];
+
+    
+    $('li:not(.none)').each(function(){
+            li_content = $(this).find('.content').text();
+            content_arr.push(li_content);
+        }); 
+    
+    
 
     $('.add').click(function create_todo() {
         new_todo = $('.addtodo').val();
         new_todo_format = $('.none').clone().removeClass('none');
         new_todo_format.find('.content').text(new_todo);
-        $('ul').append(new_todo_format);
+        $('li.none').before(new_todo_format);
         $('input.addtodo').val('');
+        content_arr.push(new_todo);
+        console.log(content_arr);
     });
     
-    // $('li').each(function(){
-        //     console.log($(this).find('.content').text());
-        // });        
         
         
     $('.todos').on('click','.icon-pencil', function edit_todo() {
@@ -32,6 +35,11 @@ $(document).ready(function() {
     $('.todos').on('click','.icon-floppy', function save_todo() {
         $(this).removeClass('icon-floppy save').addClass('icon-pencil');
         $(this).siblings('.content').attr('contenteditable','false');
+        let li_index = $(this).closest('li').index();
+        edit_todo = $(this).siblings('.content').text();
+        content_arr[li_index] = edit_todo;
+        console.log(content_arr);
+
     });
     
     $('.todos').on('mousedown','.icon-floppy', function() {
@@ -41,13 +49,12 @@ $(document).ready(function() {
 
     $('.todos').on('focusout','.content',function blur_todo (e) { 
         //     // if save->edit_todo
-        
-            if(!$(this).siblings().hasClass('save')){       
-            $(this).text(current_content);
-            $(this).siblings('.edit').removeClass('icon-floppy').addClass('icon-pencil');
-            $(this).attr('contenteditable','false');
-            }   
-        });
+        if(!$(this).siblings().hasClass('save')){       
+        $(this).text(current_content);
+        $(this).siblings('.edit').removeClass('icon-floppy').addClass('icon-pencil');
+        $(this).attr('contenteditable','false');
+        }   
+    });
 
     $('.todos').on('click','.complete', function complete_todo() {
         if ($(this).hasClass('icon-check-empty')) 
@@ -61,7 +68,10 @@ $(document).ready(function() {
     
 
     $('.todos').on('click' , '.delete' , function delete_todo() {
+        let li_index = $(this).closest('li').index();
+        content_arr.splice(li_index,1);
         $(this).closest('li').remove();
+        console.log(content_arr);
     });
 
 
