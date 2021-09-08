8ad60206038d088b3ce5df1c8cde80ea2ceb8759
meow0516 <76482269+meow0516@users.noreply.github.com>
2021-09-08 23:50:49 +0800

    update Todo on HTML vue ver.

--- js/vue_todolist.js
+++ js/vue_todolist.js
@@ -1,24 +1,41 @@
+// Vue.directive('focus', {
+//     componentUpdated: function (el, binding) {
+//       if( binding.value){
+//         // console.log(binding.value);  
+//         el.focus();
+//       }
+//     }
+//   })
+
 var todoList = new Vue({
     el: '#todolist',
     data: {
         input_todo: "",
+        editTodoId: null,
+        saveStatus: null,
+        currentTodo: "",
         todos:[
             {
                 id: "",
                 description: "123",
                 complete: false,
+                
             },
             {
                 id: "",
                 description: "abc",
                 complete: true,
+                
             },
         ],
     },
 
     methods: {
+        getAllTasks(){
+
+        },
+ 
         createTodo(){
-            // console.log('as');
         // add new todo
         this.todos.push({
             id: "",
@@ -29,6 +46,113 @@ var todoList = new Vue({
         // addTask(new_todo);
 
         this.input_todo = "";
+        },
+
+        editTodo(todo, index){
+            // change icon
+            // icon = edit now
+            if( this.editTodoId !== index){
+                if( this.editTodoId === null){
+                    // change icon = save
+    
+                    this.editTodoId = index;    
+                    this.currentTodo = todo.description;
+    
+                    // focus on input
+                    var currentEditItem = this;
+                    this.$nextTick(function(){
+                        currentEditItem.$refs['editItem'][0].focus();
+                    })
+    
+                    
+                }
+
+                else{
+                    this.todos[this.editTodoId]['description'] = this.currentTodo;
+                    // console.log('before'+this.editTodoId+this.currentTodo);
+                    
+                    this.editTodoId = index;
+                    this.currentTodo = todo.description;
+                    // console.log('after'+this.editTodoId+this.currentTodo);
+                    
+
+                    var currentEditItem = this;
+                    this.$nextTick(function(){
+                        currentEditItem.$refs['editItem'][0].focus();
+                    })
+
+                    
+
+                }
+                
+            }
+
+            else{
+                // change icon = edit
+                this.editTodoId = null;
+                // this.saveStatus = true;
+
+                
+                // save edit content by vmodel
+
+                
+            }
+
+           
+            // if( !('contenteditable' in this.todos[editTodoId])){
+            //     this.todos[editTodoId] = Object.assign({},this.todos[editTodoId],{contenteditable:true});
+            //     this.todos.splice(editTodoId,1,this.todos[editTodoId]);
+
+            // }
+            // else{
+            //     this.$delete(this.todos[editTodoId],'contenteditable');
+            //     // this.tods[editTodoId]['description'] = currentTodo;
+            //     console.log(this.todos[editTodoId]);
+            //     currentTodo = "";
+            //     console.log(currentTodo);
+            //     console.log(editTodoId);
+            // }
+ 
+            
+        },
+
+        addSaveStatus(index){
+            if( this.editTodoId !== index){
+                if( this.editTodoId === null){
+                    this.saveStatus = null;
+                    console.log('change to edit mode dont add save '+this.saveStatus);
+                }
+
+                else{
+                    this.saveStatus = null;
+                    console.log('channge to other item dont save '+this.saveStatus);
+                }
+                
+            }
+            
+            else{
+                this.saveStatus = true;                
+                console.log('change status = save '+this.saveStatus);
+            }
+
+        },
+
+        blurTodo(){
+
+            if(this.saveStatus === true){
+                console.log('save');
+                this.saveStatus = null;
+                
+            }
+            else{
+                this.todos[this.editTodoId]['description'] = this.currentTodo;
+                this.editTodoId = null;
+                this.currentTodo = "";
+                console.log('dont save');
+                this.saveStatus = null;
+                
+            }
+            
         },
 
         completeTodo(index){
@@ -47,5 +171,6 @@ var todoList = new Vue({
             // delet on html
             this.todos.splice(index,1);
         },
-    }
+    },
+
   })
